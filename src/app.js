627b5d821311361bd6f1c27962f9f8b8e65a7f08
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2022-03-11 08:52:27 -0500

    solve error handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const createError = require('http-errors');
+let createError = require('http-errors');
 const path = require('path');
 const favicon = require('serve-favicon');
-const pgtools = require('pgtools');
+require('dotenv').config();
+//const pgtools = require('pgtools');
 
 // port
 const port = process.env.PORT || 9000;
@@ -38,23 +39,16 @@ app.use(favicon(path.join(__dirname, './public/img', 'favicon.ico')));
 app.use('/', routesProject);
 app.use('/API', routesAPI);
 
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//    next(createError(404));
-//    res.render('error.html');
-//});
-
+/// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
 // error handler
-//app.use(function(err, req, res) {
-    // set locals, only providing error in development
-//    res.locals.message = err.message;
- //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//    // render the error page
-//    res.status(err.status || 500);
-//    res.render('servererror.html');
-//});
+app.use( (err, req, res, next) => {
+    process.env.NODE_ENV === 'development' ? next(createError(err)) : next();
+}
+);
 
 // start server
 app.listen(port, () => {
--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,9 @@ const { getTimes } = require('../controllers/times');
 const { getRequest } = require('../controllers/request');
 const { getUrls } = require('../controllers/urls');
 
+// crud mongodb
+const { getCrudmongo } = require('../controllers/crudmongo');
+
 // crud
 const { getCrud } = require('../controllers/crudpostgres');
 const { getusers, getinfo, createuser, getuserbyid, updateuser, deleteuser, deleteallusers } = require('../controllers/crud/users');
@@ -16,13 +19,15 @@ const { getJson } = require('../controllers/json');
 const { getNotes } = require('../controllers/notes');
 const { getMusical } = require('../controllers/musical');
 const { getSudoku } = require('../controllers/sudoku');
-const { getCrudmongo } = require('../controllers/crudmongo');
 const { getWeather } = require('../controllers/weather');
 
 
 router.get('/times', getTimes);
 router.get('/request', getRequest);
 router.get('/urls', getUrls);
+
+// crud monogoDB
+router.get('/crudmongo', getCrudmongo);
 
 // crud
 router.get('/crud', getCrud);
@@ -41,7 +46,6 @@ router.get('/json', getJson);
 router.get('/notes', getNotes);
 router.get('/musical', getMusical);
 router.get('/sudoku', getSudoku);
-router.get('/crudmongo', getCrudmongo);
 router.get('/weather', getWeather);
 
 
